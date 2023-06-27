1f3bdc43e8c7fdfaae9333b4d79cd1521106e171
KejviBode <trainee.kejvi.bode@sigmalabs.co.uk>
2023-06-27 11:49:15 +0100

    Fix pytest error causing import module error

--- extract/top_letterboxd/test_letterboxd.py
+++ extract/top_letterboxd/test_letterboxd.py
@@ -3,14 +3,14 @@ Tests for letterboxd scraping script
 '''
 import pytest
 from letterboxd import extract_top_letterboxd_films
-from conftest import top_letterboxd_soup
+# from . import letterboxd
+# from conftest import top_letterboxd_soup
 
 
 def test_top_letterboxd_invalid():
     assert extract_top_letterboxd_films(13456) is None
 
 def test_top_letterboxd_soup_valid(top_letterboxd_soup):
-    # test_soup = top_letterboxd_soup()
     html_films = extract_top_letterboxd_films(top_letterboxd_soup)
     assert html_films[0]["film_name"] == "Spider-Man: Across the Spider-Verse"
     assert html_films[0]["film_rank"] == 1
