3656193ecb88ea781c8967bcf77ea0a5c1f64594
KejviBode <trainee.kejvi.bode@sigmalabs.co.uk>
2023-06-27 11:56:10 +0100

    Reconfigure file paths so pytest only works from root dir

--- extract/top_letterboxd/conftest.py
+++ extract/top_letterboxd/conftest.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 @pytest.fixture
 def top_letterboxd_soup():
-    with open("letterboxd_html_test.html", 'r', encoding='utf-8') as page:
+    with open("./extract/top_letterboxd/letterboxd_html_test.html", 'r', encoding='utf-8') as page:
         html_bytes = page.read()
         soup = BeautifulSoup(html_bytes, "html.parser")
     return soup
